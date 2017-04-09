dff9e4605bb1ecedb4d1f5f18f27954454b407f9
DESKTOP-EN73C11\Alina <alina.capota@gmail.com>
2017-04-09 19:21:36 +0300

    Agenda implemented

---
+++ age.js
@@ -0,0 +1,30 @@
+var app=angular.module("app",[]);
+app.controller("HelloController",["$scope",function($scope){
+	$scope.contacte=[];
+function Contact(nume,prenume,telefon,mail){
+
+		this.nume=nume;
+		this.prenume=prenume;
+		this.telefon=telefon;
+		this.mail=mail;
+	};
+	
+	
+	$scope.saveC=function() {
+		alert("Adaugat cu succes!");
+		$scope.contacte.push(new Contact($scope.nume,$scope.prenume,
+			$scope.telefon,$scope.mail));
+		$scope.$apply();
+		$scope.nume=$scope.prenume=$scope.telefon=$scope.mail="";
+	}
+	$scope.sortC=function() {
+		
+	$scope.contacte.sort(function(a,b){
+	return b.nume-a.nume;
+	});
+
+	}
+
+
+}])
+	
